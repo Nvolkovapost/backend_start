a16653b24d058fffb2a9f9feff4f7f09e5438f9b
NVolkova <nvolkovapost@gmail.com>
2025-12-26 16:53:45 +0300

    Вынесла приветствие в функцию greet

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 print("Hello backend")
-print("What's your name?")
-nm = input()
-print(f"Nice to meet you, {nm}!")
+
+
+def greet():
+    name = input("What's your name?")
+    print(f"Nice to meet you, {name}!")
+
+
+greet()
